6106a114d7df665000b8a6db44d76b148ff789c4
priscagunawan <94964809+priscagunawan@users.noreply.github.com>
2021-11-25 18:52:59 +0700

    Add files via upload

---
+++ views.py
@@ -0,0 +1,8 @@
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
