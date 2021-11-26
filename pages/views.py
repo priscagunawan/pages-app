0bace96687bdaf8379ec137c3e8ffcef9f67d979
Prisca <prisca.lovely.stu@elyon.sch.id>
2021-11-26 11:33:01 +0700

    heroku

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,6 @@
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
+    template_name = 'home.html'
 class AboutPageView(TemplateView):
-    template_name = 'pages/about.html'
+    template_name = 'about.html'
